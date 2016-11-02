cfbaca8a4a16185936aca6370e321887589627c7
Susanne Stubberud Rom <susannesrom@gmail.com>
2016-11-02 18:44:38 +0100

    Begynt på footer

---
+++ js/footer.js
@@ -0,0 +1,16 @@
+var footer = document.getElementById("footer");
+
+
+var content;
+
+content = '<div class="ourfooter" id="footer">'
++         '<ul><li id="footerheader"> KONTAKT OSS </li>'
++         '<li> Fredriksten Hotell</li>'
++         '<li>Generalveien 25-27</li>'
++         '<li><a href="index.html"><img src="img/logo/LogoHvit.png" height="30" width="156"></a></li>'
++         '<li>1769 Halden, Norway</li>'
++         '<li>+ 47 69 02 10 10</li>'
++         '<li>post@fredrikstenhotell.no</li>'
+
+
+footer.innerHTML += content;

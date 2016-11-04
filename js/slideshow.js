b12c353397f1edad081a81ff03f5b4a09686b96c
susannesrom <susanner@stud.ntnu.no>
2016-11-04 11:39:19 +0100

    Add files via upload

---
+++ js/slideshow.js
@@ -0,0 +1,44 @@
+
+
+var index = 0;
+frontpageSlideshow();
+
+function frontpageSlideshow() {
+  if (document.querySelector('.bigPicture') !== null){
+    var j;
+    var x = document.getElementsByClassName("bigPicture");
+    for (j = 0; j < x.length; j++) {
+      x[j].style.display = "none";
+    }
+    index++;
+    if (index >= x.length) {index = 1}
+    x[index-1].style.display = "block";
+    setTimeout(frontpageSlideshow, 3000); // Change image every 2 seconds
+  }
+}
+
+//Slideshow for gallery:
+var slideIndex
+showSlide(slideIndex);
+
+function plusDivs(n) {
+    showSlide(slideIndex += n);
+}
+
+function setSlideIndex(n){
+  slideIndex = n;
+  showSlide(slideIndex)
+}
+
+function showSlide(n) {
+    var i;
+    var pics = document.getElementsByClassName("big");
+
+    if (n > pics.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = pics.length} ;
+    for (i = 0; i < pics.length; i++) {
+        pics[i].style.display = "none";
+    }
+    pics[slideIndex-1].style.display = "block";
+
+}

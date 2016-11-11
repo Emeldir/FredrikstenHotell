ae26e1a6d7a244f18579a58ec8dc2ce89eac0232
sunnnis <sun.johansen@gmail.com>
2016-11-11 11:01:05 +0100

    Div

---
+++ js/seasons.js
@@ -0,0 +1,18 @@
+//FILE NAME: seasons.js
+//WRITTEN BY: Sunniva Johansen
+//WHEN: November 2016
+//PURPOSE: Change content in "matogdrikke" and "aktiviteter" depending on the seasons.
+
+//Seasons:
+
+var month = d.getMonth() + 1;
+
+if (month >= 3 && month <= 5) {
+   document.getElementById("meny").innerHTML = "Paragraph changed!";
+} else if (month >= 6 && month <= 8) {
+    document.getElementById("meny").innerHTML = "Paragraph changed!";
+} else if (month >= 9 && month <= 11) {
+    document.getElementById("meny").innerHTML = "Paragraph changed!";
+} else(month == 12 || month <= 2) {
+   document.getElementById("meny").innerHTML = "Paragraph changed!";
+}

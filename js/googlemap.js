e8dd6d352371d0c5b423327084d61e300d83eb91
sunnnis <sun.johansen@gmail.com>
2016-11-11 15:20:51 +0100

    Nå er vi ikke i Australia lenger

--- js/googlemap.js
+++ js/googlemap.js
@@ -1,5 +1,5 @@
  function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
+        var uluru = {lat: 59.1161757, lng: 11.3923223};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
           center: uluru

edf8850e1ded44bac0d4c64f25b535b878ca8244
Emeldir <cille.kristoffersen@gmail.com>
2016-11-12 18:40:57 +0100

    burger meny

--- js/importNavBar.js
+++ js/importNavBar.js
@@ -10,21 +10,21 @@ var navbar = document.getElementById("navbar");
 
 var content;
 
-content = '<nav> <div class="nav-responsive">'
+content = '<nav class="mainNav">'
                +'<a class="icon" href="javascript:void(0);" style="font-size:20px;" onclick="IconClick()">☰</a>'
-               +'<a class="logo" href="index.html"><img src="img/logo/FredrikstenLogo.svg"></a></div>'
+               +'<a class="logo" href="index.html"><img src="img/logo/FredrikstenLogo.svg"></a></nav>'
            +'<ul class="topnav" id="topNavbar">'
                +'<li><a href="index.html">HOTELLET</a></li>'
                +'<li><a href="romogsuiter.html">ROM OG SUITER </a></li>'
                +'<li><a href="konferanse.html">KONFERANSE </a></li>'
-               +'<li><a href="index.html"><img src="img/logo/FredrikstenLogo.svg"></a></li>'
+               +'<li class ="logo_center"><a href="index.html"><img src="img/logo/FredrikstenLogo.svg"></a></li>'
                +'<li id="opplevelser"><a >OPPLEVELSER </a></li>'
                +'<li class="opplevelser_links"><a  href="opplevelser_matogdrikke.html"> MAT OG DRIKKE </a></li>'
                +'<li class="opplevelser_links"><a  href="opplevelser_selskap.html"> SELSKAP </a></li>'
                +'<li class="opplevelser_links"><a  href="opplevelser_aktiviteter.html"> AKTIVITETER </a></li>'
                +'<li><a href="galleri.html">GALLERI</a></li>'
                +'<li><a href="kontakt.html">KONTAKT</a></li>'
-               +'<li class="BookRom"><a href="http://online.techotel.dk/domino.aspx?hotelid=76955&lang=no" target="_blank">BOOK ROM</a></li></ul></nav>'
+               +'<li class="BookRom"><a href="http://online.techotel.dk/domino.aspx?hotelid=76955&lang=no" target="_blank">BOOK ROM</a></li></ul>'
 // Code for extra navbar under Opplevelser. Written by Torstein.
            +'<div  id="opplevelser_bar" class="opplevelser" >'
 +         '<ul>'
@@ -34,32 +34,31 @@ content = '<nav> <div class="nav-responsive">'
 
 //Written by Cecilie
 navbar.innerHTML += content;
-console.log('kake')
 
 
 
 function IconClick(){
 		var x = document.getElementById("topNavbar");
 		if(x.className === "topnav"){
-			x.className += "responsive";
-			console.log('8')
+			x.className += " responsive";
+			
 		}else {
 			x.className = "topnav";
-			console.log('10')
+	
 		}
-	};console.log('18')
+	};
 
 
 // Code for extra navbar under Opplevelser. Written by Torstein.
 
 var opplevelser_bar = document.getElementById("opplevelser_bar");
-console.log('88')
+
 
 opplevelser.addEventListener("mouseenter", function( event ) {
     opplevelser_bar.style.display = "block";
 
-});console.log('over')
+});
 opplevelser_bar.addEventListener("mouseleave", function( event ) {
     opplevelser_bar.style.display = "none";
 })
-console.log('mers')
+
